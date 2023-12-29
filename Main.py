ac818d5d8d42f80fe06382836934952f6a5bdcae
lindanceleaf <48523813+lindanceleaf@users.noreply.github.com>
2023-12-29 18:03:35 +0800

    add Main

--- Main.py
+++ Main.py
@@ -2,23 +2,30 @@ from seperate import separate
 from getFreq import getFreq
 from getSheet import getSheet
 from audioInfo import getInfo
+from choose_mode import choose_mode, get_option, kill_window
+from choose_path import select_file
 
 def main():
     # get audio
-    filename = 'Venus.mp3'
+    filename = select_file()
+    if not filename: return
     audioPath = f'audio/{filename}'
+    print(audioPath)
+
+    choose_mode()
+    kinds = get_option()
+    kill_window()
 
     # separate the audio 
     separate(audioPath)
 
-    kinds = ['piano', 'vocals', 'drums', 'bass', 'other']
     file = filename[:filename.find('.')]
     BPM, sr = getInfo(audioPath)
     for kind in kinds:
         splitPath = f'result/{file}/{kind}.wav'
         getFreq(splitPath, BPM, sr) # put the file in result/file/kind_Freq.txt
         FreqPath = f'result/{file}/{kind}_Freq.txt'
-        getSheet(FreqPath)
+        getSheet(FreqPath, kind)
 
 if __name__ == "__main__":
     main()
